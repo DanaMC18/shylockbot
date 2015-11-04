0a809b3430b332449cbbcd52b32ab334c63b3393
Dana Czinsky <DanaCzinsky@gmail.com>
2015-11-04 15:42:05 -0500

    half of act 2 completed

--- shylock.js
+++ shylock.js
@@ -1,33 +1,60 @@
 $(document).ready(function(){
 
 var $log = $('#log');
+var $logItem = $('<li>');
 var $prompt = $('#prompt');
 
-var $shylockBot = {
-  purse: 10000,
-}
-
+var $shylockBotPurse = 10000;
+var $antonioPurse = 0;
 
 
 //ACT 1
 
-var $logResponse = function(event){
+var logResponse = function(event){
   if (event.keyCode === 13) {
     if ($prompt.val() === 'purse'){ 
-      var $logItem = $('<li>');
-      $log.append($logItem.text($prompt.val()).addClass('command'))
-      $prompt.text('');
+      $log.append($logItem.text($prompt.val()).addClass('command'));
 
       var $actionItem = $('<li>').addClass('action');
-      $actionItem.text('ShylockBot pulls out his purse containing ' + $shylockBot.purse + ' ducats');
+      $actionItem.text('ShylockBot pulls out his purse containing ' + $shylockBotPurse + ' ducats');
       $log.append($actionItem);
+    
+//ACT 2
+
+    } else if ($prompt.val()[0] === 'l' && $prompt.val()[3] === 'n') {
+      var loanArray = $prompt.val().split(' ');
+        if (loanArray[1] === 'antonio') {
+          $log.append($logItem.text($prompt.val()).addClass('command'));
+        
+          var $actionItem = $('<li>').addClass('action');
+          $actionItem.text('ShylockBot gives ' + loanArray[1] + ' ' + loanArray[2] + ' ducats.');
+          $log.append($actionItem);
+
+
+          $shylockBotPurse = $shylockBotPurse - parseInt(loanArray[2]);
+          console.log('ShylockBot\'s current purse is: ' + $shylockBotPurse + ' ducats');
+          $antonioPurse = $antonioPurse + parseInt(loanArray[2]);  
+          console.log('Antonio\'s current purse is: ' + $antonioPurse + ' ducats');
+      }
+
+
     }
-  }
+
+}
 }
 
-
-
-$prompt.on('keypress', $logResponse);
+$prompt.on('keypress', logResponse);
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
