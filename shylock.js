9d3ad137b1b6fd41ff076ea95f057ea9b809f4c3
Dana Czinsky <DanaCzinsky@gmail.com>
2015-11-04 20:42:29 -0500

    cleaned up code; completed act 3

--- shylock.js
+++ shylock.js
@@ -1,12 +1,37 @@
 $(document).ready(function(){
 
 var $log = $('#log');
-var $logItem = $('<li>');
 var $prompt = $('#prompt');
+
+var $logItem = $('<li>');
+
 
 var $shylockBotPurse = 10000;
 var $antonioPurse = 0;
 var $bassanioPurse = 0;
+
+
+
+//ACT 3 
+var printLedger = function(){
+  var $actionItem = $('<li>').addClass('action');
+  $actionItem.text('ShylockBot pulls out his ledger');
+  $log.append($actionItem);
+
+  var $responseItem = $('<li>').addClass('response');
+  $responseItem.text('Antonio owes me ' + $antonioPurse + ' ducats.');
+  $log.append($responseItem);
+  
+  var $responseItem2 = $('<li>').addClass('response');
+  $responseItem2.text('Bassanio owes me ' + $bassanioPurse + ' ducats.')
+  $log.append($responseItem2);
+
+  console.log('Antonio\'s current purse is: ' + $antonioPurse + ' ducats');
+  console.log('Bassanio\'s current purse is: ' + $bassanioPurse + ' ducats');  
+  console.log('ShylockBot\'s current purse is: ' + $shylockBotPurse + ' ducats');
+  }
+
+
 
 //ACT 2
 var loanResponse = function(){
@@ -19,29 +44,24 @@ var loanResponse = function(){
         var $actionItem = $('<li>').addClass('action');
         $actionItem.text('ShylockBot gives ' + loanArray[1] + ' ' + loanArray[2] + ' ducats.');
         $log.append($actionItem);
-
-
-        $shylockBotPurse = $shylockBotPurse - parseInt(loanArray[2]);
-        console.log('ShylockBot\'s current purse is: ' + $shylockBotPurse + ' ducats');
         
         $antonioPurse = $antonioPurse + parseInt(loanArray[2]);  
         console.log('Antonio\'s current purse is: ' + $antonioPurse + ' ducats');
 
-      } else if (loanArray[1] === 'bassanio') {
-          $log.append($logItem.text($prompt.val()).addClass('command'));
+    } else if (loanArray[1] === 'bassanio') {
+        $log.append($logItem.text($prompt.val()).addClass('command'));
+      
+        var $actionItem = $('<li>').addClass('action');
+        $actionItem.text('ShylockBot gives ' + loanArray[1] + ' ' + loanArray[2] + ' ducats.');
+        $log.append($actionItem);
         
-          var $actionItem = $('<li>').addClass('action');
-          $actionItem.text('ShylockBot gives ' + loanArray[1] + ' ' + loanArray[2] + ' ducats.');
-          $log.append($actionItem);
-
-
-          $shylockBotPurse = $shylockBotPurse - parseInt(loanArray[2]);
-          console.log('ShylockBot\'s current purse is: ' + $shylockBotPurse + ' ducats');
-          
-          $bassanioPurse = $bassanioPurse + parseInt(loanArray[2]);
-          console.log('Bassanio\'s current purse is: ' + $bassanioPurse + ' ducats');      
-}
-}
+        $bassanioPurse = $bassanioPurse + parseInt(loanArray[2]);
+        console.log('Bassanio\'s current purse is: ' + $bassanioPurse + ' ducats');          
+    }
+    
+    $shylockBotPurse = $shylockBotPurse - parseInt(loanArray[2]);
+    console.log('ShylockBot\'s current purse is: ' + $shylockBotPurse + ' ducats');
+  }
 }
 
 
@@ -58,9 +78,14 @@ var logResponse = function(event){
     
 //ACT 2
     } else if ($prompt.val()[0] === 'l' && $prompt.val()[3] === 'n') {
-      loanResponse();
+      loanResponse(); //invoke ACT 2 function from above
+    
+ //ACT 3   
+    } else if ($prompt.val() === 'ledger') {
+      $log.append($logItem.text($prompt.val()).addClass('command'));
+      printLedger(); //invoke ACT 3 function from above
     }
-
+    $prompt.val('');
   }
 }
 
